164a4ee1f4e475eaf12bf23e7099fbe9b893b82a
ChristopherNagl <christophernagl33@gmail.com>
2024-08-17 13:36:04 +0200

    docs: extend example for worksapce_user_assignment

--- examples/resources/microsoftfabric_workspace_user_assignment/resource.tf
+++ examples/resources/microsoftfabric_workspace_user_assignment/resource.tf
@@ -3,8 +3,14 @@ resource "microsoftfabric_workspace_user_assignment" "example_assignment" {
 
   users = [
     {
-      email = "AdeleV@3cg7y4.onmicrosoft.com"
-      role  = "Member"
+      email          = "AdeleV@3cg7y4.onmicrosoft.com"
+      role           = "Member"
+      principal_type = "User"
+    },
+    {
+      email          = "f4c6053c-5243-4690-9e1f-f1b5a7558202"
+      role           = "Contributor"
+      principal_type = "Group"
     }
   ]
 }
