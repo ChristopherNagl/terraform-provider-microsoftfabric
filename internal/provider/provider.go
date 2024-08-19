a6c65dbb7978ef2193191257ccce05757c07c188
ChristopherNagl <christophernagl33@gmail.com>
2024-08-19 21:28:28 +0200

    feat: add workspace spark pool resource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -121,5 +121,6 @@ func (p *microsoftFabricProvider) Resources(_ context.Context) []func() resource
 		func() resource.Resource { return NewSemanticModelUserAssignmentResource(p.client) },
 		func() resource.Resource { return NewDomainResource(p.client) },
 		func() resource.Resource { return NewLakehouseResource(p.client) },
+		func() resource.Resource { return NewSparkPoolResource(p.client) },
 	}
 }
